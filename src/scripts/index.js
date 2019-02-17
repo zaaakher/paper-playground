2a622bb7edd7fb77e5c6710dfaedcbb2f34d1cc8
Zakher Masri <masrizakher@gmail.com>
2019-02-17 22:48:29 +0300

    wobble design

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,29 +1,53 @@
 import "../styles/index.scss";
 import Paper from "paper";
 import Kolor from "kolorwheel";
+import { getRandomInt, getRandomArbitrary } from "./util";
 
+let w = 1080;
+let h = 1080;
 let canv = document.getElementById("canv");
-canv.setAttribute("width", 500);
-canv.setAttribute("height", 500);
+canv.setAttribute("width", w);
+canv.setAttribute("height", h);
 
 Paper.setup(canv);
-let bg = new Paper.Path.Rectangle(new Paper.Point(0, 0), 500);
-// bg.fillColor = "lightblue";
-let cir = new Paper.Path.Circle(bg.bounds.center, 10);
-// cir.fillColor = "red";
-var text = new Paper.PointText(bg.bounds.center);
-text.justification = "center";
-text.content = "Text";
-text.fontSize = 20;
-text.fontWeight = 800;
-text.bounds.center.set(bg.bounds.center);
+let bg = new Paper.Path.Rectangle(new Paper.Point(0, 0), new Paper.Size(w, h));
+bg.fillColor = "black";
 
-let baseColor = new Kolor("#C0DA74");
-text.fillColor = baseColor.getHex();
-for (let i = 0; i < 10; i++) {
-  baseColor.h += 20;
-  let t = text.clone();
-  t.fillColor = baseColor.getHex();
-  t.position.x += i * 5;
-  t.position.y += i * 5;
+let ref = Paper.project.view;
+
+let cir = new Paper.Path.Circle(ref.bounds.center, 30);
+// cir.strokeColor = "red";
+// cir.strokeWidth = 5;
+
+let anchors = [];
+let divisions = getRandomInt(5, 20);
+
+for (let i = 0; i < divisions; i++) {
+  let line = new Paper.Path.Line(
+    ref.bounds.center,
+    cir.getPointAt((cir.length / divisions) * i)
+  );
+
+  line.scale(getRandomArbitrary(1, 1.3), ref.bounds.center);
+  let dot = line.lastSegment;
+  anchors.push(dot);
+
+  line.remove();
+}
+
+// console.log(anchors);
+let path = new Paper.Path(anchors);
+// path.bounds.center.set(ref.bounds.center);
+// path.strokeColor = "black";
+// path.strokeWidth = 5;
+path.closed = true;
+path.smooth();
+for (let i = 0; i < 50; i++) {
+  let clonedPath = path.clone();
+  clonedPath.scale(0.2 * i);
+  clonedPath.strokeColor = "white";
+  clonedPath.strokeWidth = 0.1 * i;
+  clonedPath.onFrame = function() {
+    this.rotate(0.01 * i);
+  };
 }
---
+++ src/scripts/util.js
@@ -0,0 +1,82 @@
+import Paper from "paper";
+import * as d3 from "d3";
+
+const createCanvas = (w, h) => {
+  //Create Canvas
+  let canvas = d3
+    .select("body") //Select body element
+    .append("canvas") //Append a canvas element
+    .attr("class", "canv")
+    .attr("width", w) //set width
+    .attr("height", h); // set height
+  // console.log(canvas.node().offsetWidth);
+  canvas.node().style.width = w + "px";
+  canvas.node().style.height = h + "px";
+
+  // canvas.node().offsetHeight
+  return canvas;
+};
+const setupCanvas = (canvW, canvH) => {
+  new Paper.Project(createCanvas(canvW, canvH).node());
+  new Paper.Path.Rectangle(0, 0, canvW, canvH);
+};
+const createSliders = defaultValues => {
+  //New div to contain the sliders
+  let controls = d3.select("body").append("div");
+  controls.attr("class", "controls");
+
+  //Create slider divs and connect data
+  let sliders = d3
+    .select(".controls") //select parent element
+    .selectAll("div")
+    .data(Object.keys(defaultValues))
+    .enter()
+    .append("div"); //Add a div for each item in the data
+
+  //Create text for slider label
+  sliders.append("text").text(d => defaultValues[d].title);
+  //Create sliders
+  sliders
+    .append("input") //n
+    .attr("type", "range")
+    .attr("min", d => defaultValues[d].min) //min value
+    .attr("value", d => defaultValues[d].val)
+    .attr("max", d => defaultValues[d].max); //max value
+};
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+var downloadAsSVG = function(fileName) {
+  if (!fileName) {
+    fileName = "paperjs_example.svg";
+  }
+  var url =
+    "data:image/svg+xml;utf8," +
+    encodeURIComponent(Paper.project.exportSVG({ asString: true }));
+
+  var link = document.createElement("a");
+  link.download = fileName;
+  link.href = url;
+  link.click();
+};
+
+const getRandomElement = arr => arr[getRandomInt(0, arr.length - 1)];
+const remapNumbers = (value, sourceRange, targetRange) => {
+  let oldRange = sourceRange[1] - sourceRange[0];
+  let newRange = targetRange[1] - targetRange[0];
+  return ((value - sourceRange[0]) * newRange) / oldRange + targetRange[0];
+};
+
+export {
+  getRandomElement,
+  remapNumbers,
+  getRandomInt,
+  downloadAsSVG,
+  getRandomArbitrary,
+  createSliders,
+  createCanvas,
+  setupCanvas
+};
